e24b6e5cf9e8c6f2fa164e3e84a43219b4690d66
wjq <wjq@163.com>
2019-12-18 22:18:30 +0800

    添加springcloud config相关代码实现

--- greenwich-sr4-order/src/main/java/cn/com/wjqhuaxia/controller/HelloOrderController.java
+++ greenwich-sr4-order/src/main/java/cn/com/wjqhuaxia/controller/HelloOrderController.java
@@ -1,5 +1,7 @@
 package cn.com.wjqhuaxia.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import cn.com.wjqhuaxia.client.StockMsgClient;
 @RestController
 public class HelloOrderController {
 	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private StockMsgClient stockMsgClient;
 	@Autowired
@@ -20,6 +24,7 @@ public class HelloOrderController {
 	@GetMapping("/testStockRequest")
 	@ResponseBody
 	public String testStockRequest(){
+		logger.info("testStockRequest start...");
 		return "order service: " + stockMsgClient.getStockMsg();
 	}
 	
